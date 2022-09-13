5d682b1a5a58391207ab5ac39bc596f0d628f394
JatinBumbra <jatin.bumbra.23@gmail.com>
2022-09-13 20:27:21 +0530

    Create the smart contract and a basic dummy UI

--- contracts/Contact.sol
+++ contracts/Contact.sol
@@ -7,12 +7,73 @@ import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 
-// This is an example contract. Please remove and create your own!
-contract Contact is Initializable, UUPSUpgradeable, OwnableUpgradeable{
+/*
+    REQUIREMENTS
+    - A counter to keep track of contact IDs
+    - A data structure to hold Contacts
+    - A mapping to map Contacts to each address
+    - A method to return contacts for msg.sender
+    - A method to add contacts for msg.sender
+    - A method to delete contacts for msg.sender
+*/
+contract Contacts is Initializable, UUPSUpgradeable, OwnableUpgradeable{
+    using Counters for Counters.Counter;
+    Counters.Counter internal _contactIds;
+
+    struct Contact {
+        uint256 id;
+        address contact;
+    }
+    mapping(address => Contact[]) contacts;
+
+    event ContactAdded (address contact, uint256 id);
+    event ContactDeleted (address contact);
+
     function initialize() public initializer onlyProxy {
         __Ownable_init();
         __UUPSUpgradeable_init();
     }
 
     function _authorizeUpgrade(address) internal override onlyOwner {}
+
+    // A method to return contacts for msg.sender
+    function getContacts() public view returns(Contact[] memory) {
+        return contacts[msg.sender];
+    }
+
+    // A method to add contacts for msg.sender
+    function addContact(address _contact) public payable {
+        Contact[] storage userContacts = contacts[msg.sender];
+        for(uint256 i=0; i<userContacts.length; i++) {
+            if(userContacts[i].contact == _contact) revert("Contact already exists");
+        }
+
+        _contactIds.increment();
+        Contact memory temp = Contact({
+            id: _contactIds.current(),
+            contact: _contact
+        });
+        userContacts.push(temp);
+        emit ContactAdded(temp.contact,temp.id);
+    }
+
+    // A method to delete contacts for msg.sender
+    function deleteContact(address _contact) public payable {
+        Contact[] storage userContacts = contacts[msg.sender];
+
+        int256 index = -1;
+        for(uint256 i=0; i<userContacts.length; i++) {
+            if(userContacts[i].contact == _contact) {
+                index = int256(i);
+                break;
+            }
+        }
+        if(index == -1) revert("Contact does not exist");
+
+        for(uint256 i=uint256(index); i<userContacts.length-1; i++) {
+            userContacts[i] = userContacts[i+1];
+        }
+        userContacts.pop();
+        emit ContactDeleted(_contact);
+    }
 }
--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,132 @@
+import { ReactNode } from 'react';
+// MUI
+import Box from '@mui/material/Box';
 import Container from '@mui/material/Container';
+import Divider from '@mui/material/Divider';
+import Grid from '@mui/material/Grid';
+import InputAdornment from '@mui/material/InputAdornment';
+import IconButton from '@mui/material/IconButton';
+import Paper from '@mui/material/Paper';
+import TextField from '@mui/material/TextField';
 import Typography from '@mui/material/Typography';
+// Icons
+import AccountBoxIcon from '@mui/icons-material/AccountBox';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import DeleteIcon from '@mui/icons-material/Delete';
+import PersonAddIcon from '@mui/icons-material/PersonAdd';
+import SearchIcon from '@mui/icons-material/Search';
+
+const SectionTitle = ({
+  children,
+  icon,
+}: {
+  children: ReactNode;
+  icon: any;
+}) => (
+  <Box display='flex' alignItems='center' my={1.5}>
+    {icon}
+    <Typography variant='h6' ml={1}>
+      {children}
+    </Typography>
+  </Box>
+);
+
+const ContactListItem = ({
+  children,
+  icon,
+}: {
+  children: ReactNode;
+  icon: any;
+}) => (
+  <Box mb={1}>
+    <Paper variant='outlined'>
+      <Box display='flex' alignItems='center' p={2} pr={1} bgcolor='#f4f4f4'>
+        <AccountCircleIcon fontSize='large' />
+        <Typography ml={1} flex={1}>
+          {children}
+        </Typography>
+        <IconButton>{icon}</IconButton>
+      </Box>
+    </Paper>
+  </Box>
+);
 
 const App = () => {
   return (
-    <Container>
-      <Typography>MUI Test</Typography>
+    <Container maxWidth='md'>
+      <Box
+        height='98vh'
+        overflow='hidden'
+        display='flex'
+        flexDirection='column'
+      >
+        <Grid container>
+          <Grid item xs={5}>
+            <SectionTitle icon={<AccountBoxIcon />}>My Contacts</SectionTitle>
+            <Divider />
+          </Grid>
+          <Grid item xs={1}></Grid>
+          <Grid item xs={5}>
+            <SectionTitle icon={<PersonAddIcon />}>
+              Add New Contact
+            </SectionTitle>
+            <Divider />
+          </Grid>
+          <Grid item xs={5} my={1.5}>
+            <TextField
+              size='small'
+              variant='outlined'
+              placeholder='Search'
+              fullWidth
+              InputProps={{
+                startAdornment: (
+                  <InputAdornment position='start'>
+                    <SearchIcon />
+                  </InputAdornment>
+                ),
+              }}
+            />
+          </Grid>
+          <Grid item xs={1}></Grid>
+          <Grid item xs={5} my={1.5}>
+            <TextField
+              size='small'
+              variant='outlined'
+              placeholder='Search'
+              fullWidth
+              InputProps={{
+                startAdornment: (
+                  <InputAdornment position='start'>
+                    <SearchIcon />
+                  </InputAdornment>
+                ),
+              }}
+            />
+          </Grid>
+        </Grid>
+
+        <Grid container flex={1} height='100%' overflow='hidden'>
+          <Grid item xs={5} height='100%' overflow='scroll'>
+            {Array(20)
+              .fill(true)
+              .map((_, i) => (
+                <ContactListItem key={i} icon={<DeleteIcon color='error' />}>
+                  Identity {i + 1}
+                </ContactListItem>
+              ))}
+          </Grid>
+          <Grid item xs={1}></Grid>
+          <Grid item xs={5} height='100%' overflow='scroll'>
+            {Array(20)
+              .fill(true)
+              .map((_, i) => (
+                <ContactListItem key={i} icon={<PersonAddIcon />}>
+                  Identity {i + 1}
+                </ContactListItem>
+              ))}
+          </Grid>
+        </Grid>
+      </Box>
     </Container>
   );
 };
