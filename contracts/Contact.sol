c9bbd2f2660a22187765e8e6edb1f40681c25696
JatinBumbra <jatin.bumbra.23@gmail.com>
2022-09-13 12:06:56 +0530

    Add typescript, modify the contract and reorganize code

---
+++ contracts/Contact.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.8.0;
+pragma experimental ABIEncoderV2;
+
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+
+// This is an example contract. Please remove and create your own!
+contract Contact is Initializable, UUPSUpgradeable, OwnableUpgradeable{
+    function initialize() public initializer onlyProxy {
+        __Ownable_init();
+        __UUPSUpgradeable_init();
+    }
+
+    function _authorizeUpgrade(address) internal override onlyOwner {}
+}
